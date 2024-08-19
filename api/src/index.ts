cea37ebff54a52e0690cb1a254c249943df3fbbb
kmark43 <kyler.markland@yahoo.com>
2024-08-18 17:27:52 -0700

    Add and remove through express

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,8 +3,28 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+type TodoItem = {
+    id: string;
+    value: string;
+}
+
+let todoItems: TodoItem[] = [];
+
+app.post('/todoAdd', (req: Request, res: Response) => {
+  todoItems.push({
+    id: req.params['id'],
+    value: req.params['value']
+  });
+  res.sendStatus(200);
+});
+
+app.post('/todoRemove', (req: Request, res: Response) => {
+  todoItems = todoItems.filter((val) => val.id !== req.params['id']);
+  res.sendStatus(200);
+});
+
+app.post('/todoList', (req: Request, res: Response) => {
+  res.send(todoItems);
 });
 
 app.listen(port, () => {
